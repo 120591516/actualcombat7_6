9fb28912363c8c6b69c2b222157ae9551079d44b
wangwenteng <wangwenteng@llvision.com>
2017-12-22 16:20:59 +0800

    socket

--- src/main/java/com/wisely/actualcombat7_6/WebSocketConfig.java
+++ src/main/java/com/wisely/actualcombat7_6/WebSocketConfig.java
@@ -1,10 +1,14 @@
 package com.wisely.actualcombat7_6;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 /*
@@ -13,7 +17,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
  * broker）的消息，这时控制器支持使用@MessageMapping，就像是使用@RequestMapping一样
  */
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer implements WebSocketConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {// 注册STOMP的协议节点（endpoint），并映射指定url
@@ -28,4 +32,22 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 		registry.enableSimpleBroker("/topic");
 	}
 
+	public WebSocketConfig() {
+	}
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(systemWebSocketHandler(), "/websck").addInterceptors(new HandshakeInterceptor());
+
+		System.out.println("registed!");
+		registry.addHandler(systemWebSocketHandler(), "/sockjs/websck").addInterceptors(new HandshakeInterceptor())
+				.withSockJS();
+
+	}
+
+	@Bean
+	public WebSocketHandler systemWebSocketHandler() {
+		// return new InfoSocketEndPoint();
+		return new SystemWebSocketHandler();
+	}
 }
